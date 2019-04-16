fc902e8de29eae5fe899e5b1da6481f5ae3384ec
Dmytro_Slobodenyuk <Dmytro_Slobodenyuk@epam.com>
2019-04-16 10:43:30 +0300

    init

---
+++ src/main/java/com/dmslob/auth/security/MyUserDetails.java
@@ -0,0 +1,38 @@
+package com.dmslob.auth.security;
+
+import com.dmslob.auth.model.User;
+import com.dmslob.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// Implements UserDetailsService in order to define our own custom loadUserbyUsername function.
+// The UserDetailsService interface is used to retrieve user-related data.
+// It has one method named loadUserByUsername() which finds a user entity based on the username and can be overridden to customize the process of finding the user.
+@Service
+public class MyUserDetails implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        }
+
+        return org.springframework.security.core.userdetails.User//
+                .withUsername(username)//
+                .password(user.getPassword())//
+                .authorities(user.getRoles())//
+                .accountExpired(false)//
+                .accountLocked(false)//
+                .credentialsExpired(false)//
+                .disabled(false)//
+                .build();
+    }
+}
